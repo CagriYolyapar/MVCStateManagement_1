551bac1368a9549b1e8ba522261eee7f663f568f
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-04-20 10:49:16 +0300

    Session ile ilgili actionlar acıldı

--- MVCStateManagement_1/Controllers/HomeController.cs
+++ MVCStateManagement_1/Controllers/HomeController.cs
@@ -30,9 +30,39 @@ namespace MVCStateManagement_1.Controllers
 
         public ActionResult SessionState()
         {
+            #region SessionState
+            //SessionState => Sunucu(server) tarafında siteye baglanan her bir kullanıcı icin olusturulan ayrı bir hafıza bölgesidir...Bu kullanıcı ile ilgili bazı bilgileri saklamak istiyorsak (giriş yapan kullanıcı nesnesi, sepeti , özel nesneler) bunu Session dedigimiz alanlarda  yaparız...Session kullanıcı bazlı olusur...Eger siz girdiginizde Session yaratılırsa o anki kullanıcıya (size) özel bir hafıza alanı yaratılır....
+            #endregion
+
+
+            Session["oturum"] = 1;
+
             return View();
         }
 
+        [HttpPost]
+        public ActionResult SessionState(string veri)
+        {
+            Session["deneme"] = veri;
+            return RedirectToAction("SessionTest");
+        }
+
+        public ActionResult SessionTest()
+        {
+            return View();
+        }
+        
+        public ActionResult LogOut()
+        {
+            //Session.Clear() bu ifade varolan bütün Session nesnelerini temizler...
+            if(Session["deneme"] != null)
+            {
+                Session.Remove("deneme"); //ilgili oturumda sadece bu key'e sahip olan Session'u temizler...
+            }
+
+            return RedirectToAction("SessionTest");
+           
+        }
        
     }
 }
